791cdefd2075a26aeae6dcb8af787e04b3afd328
HoangLong9991 <93160214+HoangLong9991@users.noreply.github.com>
2022-02-19 00:09:53 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,9 @@
+<html lang="en">
+<head>
+<meta charset = "UTF-8">
+<title> ATN Store </title>
+</head>
+<body>
+    <a href = "Login.php" > Login </a>
+</body>
+</html>
---
+++ Login.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="POST")
+{
+$uname = $_POST["username"];
+$pwd = $_POST["password"]
+}
+
+<html lang="en">
+<head>
+<meta charset = "UTF-8">
+<title> ATN Store </title>
+</head>
+<body>
+<form action="" method="post">
+    <label><b>Username</b></label> <input type="text" name="username" >
+    <label><b>Username</b></label> <input type="text" name="password" >
+    <input type = "submit" value = "Submit"/><br/>
+</form>
+</body>
+</html>
