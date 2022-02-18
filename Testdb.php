67b69aef5de7eb4c8fb3b7b358bde44a1f73147e
HoangLong9991 <93160214+HoangLong9991@users.noreply.github.com>
2022-02-19 00:38:43 +0700

    Update Testdb.php

--- Testdb.php
+++ Testdb.php
@@ -3,7 +3,7 @@
 $dbconn = pg_connect("host=ec2-18-215-8-186.compute-1.amazonaws.com dbname=deuvp2rhk0qqne user=etwobxbazzsyzv password=e533543352eb48ef8570e20e053dcc70ac4e4b26eac13c4c584ca8528c9feabe");
 
 // Performing SQL query
-$query = 'SELECT * FROM acc';
+$query = 'SELECT * FROM account';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
